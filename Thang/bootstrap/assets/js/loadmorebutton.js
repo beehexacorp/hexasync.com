0d156bc5b4e7c472ed7f3e32ea199b69f69f70dd
Thang <quang.thang@beehexa.com>
2024-08-13 09:45:38 +0700

    Updatefile-JS

---
+++ Thang/bootstrap/assets/js/loadmorebutton.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".content").slice(0, 4).show();
+    $("#loadMore").on("click", function(e){
+      e.preventDefault();
+      $(".content:hidden").slice(0, 2).show();
+
+    });
+    
+  })
