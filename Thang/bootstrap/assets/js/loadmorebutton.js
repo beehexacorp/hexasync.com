892c02014325913339fef222e825af574a8c4c74
Thang <quang.thang@beehexa.com>
2024-08-15 17:19:17 +0700

    Rebuild-HexaSync-Shopify_Integration_ver2-Afternoon

--- Thang/bootstrap/assets/js/loadmorebutton.js
+++ Thang/bootstrap/assets/js/loadmorebutton.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(".content").slice(0, 4).show();
     $("#loadMore").on("click", function(e){
       e.preventDefault();
-      $(".content:hidden").slice(0, 2).show();
+      $(".content:hidden").slice(0, 10).show();
 
     });
     
