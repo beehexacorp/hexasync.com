fefcc24e373365e1b3d2245a460a15893b991c30
Thang <quang.thang@beehexa.com>
2024-08-20 11:10:44 +0700

    Shopify-integration

--- Thang/bootstrap/assets/js/loadmorebutton.js
+++ Thang/bootstrap/assets/js/loadmorebutton.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".content").slice(0, 4).show();
+    $(".content").slice(0, 6).show();
     $("#loadMore").on("click", function(e){
       e.preventDefault();
       $(".content:hidden").slice(0, 10).show();
