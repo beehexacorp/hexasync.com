b489e62d4b0dc86fcc9b6f6898b32bf45363b82b
Thang <quang.thang@beehexa.com>
2024-08-31 16:46:49 +0700

    delete folder HexaSync

--- Thang/bootstrap/assets/js/loadmorebutton.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".content").slice(0, 6).show();
-    $("#loadMore").on("click", function(e){
-      e.preventDefault();
-      $(".content:hidden").slice(0, 10).show();
-
-    });
-    
-  })
--- Thang/bootstrap/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-var name = "John";
-let age = 30;
-const isStudent = 'cc';
-
-console.log(name, age, isStudent);
-
-document.getElementById("myButton").addEventListener("click", function() {
-    alert("Button was clicked!");
-});
