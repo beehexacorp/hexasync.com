a14d7a5110864e5eca8d0de2d8b6c7b89482576d
Thang <quang.thang@beehexa.com>
2024-08-31 16:48:53 +0700

    HexaSync.com

---
+++ Thang/bootstrap/assets/js/script.js
@@ -0,0 +1,9 @@
+var name = "John";
+let age = 30;
+const isStudent = 'cc';
+
+console.log(name, age, isStudent);
+
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Button was clicked!");
+});
